09da92fb8dce50672be9136523f3112da1736bea
sdgamboa <samuel.gamboa.tuz@gmail.com>
2024-06-20 14:33:43 -0400

    Add unit tests to make sure makeSignatures works with DataFrame

---
+++ tests/testthat/test-makeSignatures-DataFrame.R
@@ -0,0 +1,14 @@
+library(purrr)
+library(S4Vectors)
+bp <- map(importBugphyzz(), DataFrame)
+sigsNames <- map(bp, ~ makeSignatures(.x, tax_id_type = "Taxon_name")) |>
+    list_flatten(name_spec = "{inner}")
+sigsIDs <- map(bp, ~ makeSignatures(.x, tax_id_type = "NCBI_ID")) |>
+    list_flatten(name_spec = "{inner}")
+
+test_that("makeSignatures works with IDs", {
+    expect_true(all(map_lgl(sigsIDs, is.integer)))
+})
+test_that("makeSignatures works with taxon names", {
+    expect_true(all(map_lgl(sigsNames, is.character)))
+})
